82e37de7ab56560cf1522f12a3d02c615abf82f5
philipp <philipp.reiter90@gmail.com>
2022-01-28 11:56:21 +0100

    initial push

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+import * as Sentry from 'sentry-expo';
+
+Sentry.init({
+  dsn: 'XXXXX',
+  enableInExpoDevelopment: false,
+  debug: false, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+  enableNative: true,
+});
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button title="Press me for Error" onPress={() => {
+        console.log(test.unknownAttribute)
+      }} />
     </View>
   );
 }
